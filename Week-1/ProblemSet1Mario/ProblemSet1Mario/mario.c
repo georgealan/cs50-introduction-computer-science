c0f59cde481da2ec3e35d117e6226a5449ab464c
George Alan Rufo <georgealanrufo@gmail.com>
2023-06-24 19:16:09 -0300

    Organizing folders, and begin PSet 01 Mario

---
+++ Week-1/ProblemSet1Mario/ProblemSet1Mario/mario.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  int height;
+  do
+  {
+    height = get_int("Height: ");
+  } while (height < 1);
+ 
+}
---
+++ Week-4/README.md
@@ -0,0 +1,2 @@
+# cs50-introduction-computer-science
+Repository with the assignments and exercises from the Harvard CS50 course.
