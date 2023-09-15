8bb79a4f86f5eb79b8daedf0299f220b50a87a9a
George Alan Rufo <georgealanrufo@gmail.com>
2023-09-15 19:15:26 -0300

    Add all problems templates of week 2

---
+++ Week-2/PracticeProblems/NoVowels/no-vowels.c
@@ -0,0 +1,12 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <stdio.h>
+#include "cs50.h"
+
+int main(int argc, string argv[])
+{
+    
+}
